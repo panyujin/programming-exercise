2703e5cc0f93ec85ef424552a99bdaa968a4bc19
panyujin <panyujinyx@gmail.com>
2017-08-24 21:51:46 +0800

    ruby 16

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -1,11 +1,17 @@
 # 给定一阵列内含数字，输出最大值
 
 def find_max(array)
-  #....
+       max = array.first
+       (2..array.size).to_a.each do |i|
+           if max < array[i-1]
+                max = array[i-1]
+              end
+            end
+            return max
+
 end
 
 arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
 
 max = find_max(arr)
-puts "Max is #{max}" # 应该是 88
-
+puts "#{max}" # 应该是 88
